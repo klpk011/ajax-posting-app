5b3110fc5d80b323bf43b6396b262af48439a6f1
klpk011 <sophieQ011@gmail.com>
2017-04-14 21:51:37 +0800

    implement Endless Page function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,36 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:create, :destroy]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def index
-    @posts = Post.order("id DESC").all   # 新贴文放前面
+    # @posts = Post.order("id DESC").all   # 新贴文放前面
+    @posts = Post.order('id DESC').limit(20)
+
+    @posts = @posts.where('id < ?', params[:max_id]) if params[:max_id]
+
+    respond_to do |format|
+      format.html  # 如果客户端要求 HTML，则回传 index.html.erb
+      format.js    # 如果客户端要求 JavaScript，回传 index.js.erb
+    end
   end
 
   def create
     @post = Post.new(post_params)
     @post.user = current_user
     @post.save
-
-
   end
 
   def destroy
-    @post =  current_user.posts.find(params[:id]) # 只能删除自己的贴文
+    @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
     @post.destroy
     #  redirect_to posts_path
-   # render :js => "alert('ok');"
-    render :json => { :id => @post.id }
-
+    # render :js => "alert('ok');"
+    render json: { id: @post.id }
   end
 
   def like
     @post = Post.find(params[:id])
-    unless @post.find_like(current_user)  # 如果已经按讚过了，就略过不再新增
-      Like.create( :user => current_user, :post => @post)
+    unless @post.find_like(current_user) # 如果已经按讚过了，就略过不再新增
+      Like.create(user: current_user, post: @post)
     end
   end
 
@@ -33,25 +38,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     like = @post.find_like(current_user)
     like.destroy
-    render "like"
+    render 'like'
   end
 
-    def collect
+  def collect
     @post = Post.find(params[:id])
-    unless @post.find_collect(current_user)  # 如果已收藏，就略过不再新增
-      Collect.create( :user => current_user, :post => @post)
+    unless @post.find_collect(current_user) # 如果已收藏，就略过不再新增
+      Collect.create(user: current_user, post: @post)
     end
-  end
+end
 
   def uncollect
     @post = Post.find(params[:id])
     collect = @post.find_collect(current_user)
     collect.destroy
-    render "collect"
+    render 'collect'
   end
 
   private
-    def post_params
-      params.require(:post).permit(:content)
-    end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
