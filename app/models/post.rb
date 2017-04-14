87d850e7cb4ccfb2dcdad69fbf841fe1e2cc4370
klpk011 <sophieQ011@gmail.com>
2017-04-14 09:20:58 +0800

    implement create/destroy/like/collect post function

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  validates_presence_of :content
+  belongs_to :user
+  has_many :likes, :dependent => :destroy
+  has_many :liked_users, :through => :likes, :source => :user
+
+  has_many :collects, :dependent => :destroy
+  has_many :collected_users, :through => :collects, :source => :user
+  def find_like(user)
+      self.likes.where( :user_id => user.id ).first
+    end
+
+  def find_collect(user)
+    self.collects.where( :user_id => user.id ).first
+  end
+end
