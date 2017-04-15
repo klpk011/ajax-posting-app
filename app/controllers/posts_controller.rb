2307a86f0a945cdff83e9aa1eb0dc3d2c2718466
klpk011 <sophieQ011@gmail.com>
2017-04-15 11:17:05 +0800

    use jQuery Plugin star score

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,12 +70,25 @@ end
   end
 
     def update
-      # sleep(1)
+      sleep(1)
       @post = Post.find(params[:id])
       @post.update!( post_params )
 
       render :json => { :id => @post.id, :message => "ok"}
     end
+
+    def rate
+    @post = Post.find(params[:id])
+
+    existing_score = @post.find_score(current_user)
+    if existing_score
+      existing_score.update( :score => params[:score] )
+    else
+      @post.scores.create( :score => params[:score], :user => current_user )
+    end
+
+    render :json => { :average_score => @post.average_score }
+  end
   private
 
   def post_params
